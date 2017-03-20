33549cf003b9a57cd7dd29dd9cc9d7a03c36c0d7
Hieu Nguyen <hnnguyen45@gmail.com>
2017-03-20 02:16:38 -0700

    questions created

--- src/App.js
+++ src/App.js
@@ -21,18 +21,30 @@ class App extends Component {
         super(props);
 
         this.state = {
+            arts : [
+                {title:"Karate",score:0},"Mixed Martial Arts","Tae Kwon Do","Jiu-Jitsu", "Judo", "Kendo" ,"Kickboxing/Thai Boxing"
+            ],
+
             questions: [
                 {
                     id: 1,
-                    text: '',
+                    text: 'Do you prefer to strike and stand-up (punch and/or kick)?',
                     possibleAnswers: [
                         {
-                            id: 'a',
-                            text: ''
+                            
+                            text: 'yes',
+                            '0': 1,
+                            '1': 1,
+                            '2': 0,
+                            '3': 1
                         },
                         {
                             id: 'b',
-                            text: ''
+                            text: 'no',
+                            '0': 0,
+                            '1': 0,
+                            '2': 1,
+                            '3': 0
                         },
                         {
                             id: 'c',
@@ -43,7 +55,7 @@ class App extends Component {
                 },
                 {
                     id: 2,
-                    text: '',
+                    text: 'Do you feel comfortable doing grappling, submissions, or throwing your opponent ? ',
                     possibleAnswers: [
                         {
                             id: 'a',
@@ -62,7 +74,7 @@ class App extends Component {
                 },
                 {
                     id: 3,
-                    text: '',
+                    text: 'Are you okay using weapons, foreign objects, or any sharp weapons for hand to hand combat sparring ? ',
                     possibleAnswers: [
                         {
                             id: 'a',
@@ -81,7 +93,7 @@ class App extends Component {
                 },
                 {
                     id: 4,
-                    text: '',
+                    text: 'Do you like to compete in tournaments or olympics in the future? ',
                     possibleAnswers: [
                         {
                             id: 'a',
@@ -97,7 +109,26 @@ class App extends Component {
                         }
                     ],
                     
-                }
+                },
+                {
+                    id: 5,
+                    text: 'Would you like to learn how to break wooden boards, bricks, stones, etc. ? ',
+                    possibleAnswers: [
+                        {
+                            id: 'a',
+                            text: ''
+                        },
+                        {
+                            id: 'b',
+                            text: ''
+                        },
+                        {
+                            id: 'c',
+                            text: 'Schemas'
+                        }
+                    ],
+                    
+                }    
             ],
             score: 0,
             currentQuestion: 1
@@ -129,8 +160,11 @@ class App extends Component {
         }
 
         return (
-            <div className="col-md-6">
-                <h1 className="decision-heading">Martial Arts Decision</h1>
+            <div className="col-md-6 col-md-offset-3">
+                <h1 className="decision-heading">Martial Art : Decision Making</h1>
+            <p className="App-intro">
+        Interested in starting Martial Arts? Don't know which discipline to study or start! 
+        Then you come to the right place to evaluate and decide which is a perfect fit for you.</p>
                 {scoreboard}
                 <QuestionList
                     {...this.state}
