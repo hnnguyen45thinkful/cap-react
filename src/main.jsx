9f00c83c1557577a1bea09b8bd55fa16b9bba6bb
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2017-08-17 21:10:11 -0400

    Updates

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, Redirect } from 'react-router';
-
-
-
-ReactDOM.render(<Main />, document.getElementById('root'));
